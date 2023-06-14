5e744301e108c3d7c01809106fe9feaea61a99fe
Hernan Polo <hernan.polo1985@gmail.com>
2023-06-14 18:13:22 -0300

    Média com JS

---
+++ exercicio_1_04/script.js
@@ -0,0 +1,7 @@
+alert ('Bora calcular a media de tres numeros? Digite eles a continuação!')
+const numero1=prompt ("Digite o 1ro numero")
+const numero2=prompt ("Digite o 2do numero")
+const numero3=prompt ('Digite o 3ro e ultimo numero')
+const quantNum= 3
+const resultado = (Number(numero1)+Number(numero2)+Number(numero3))/quantNum
+alert ('A media dos tres numeros é: '+ resultado )
