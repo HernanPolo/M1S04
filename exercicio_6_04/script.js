222fc9502a6f9dff83e89ad1ae9267d85b426e4c
Hernan Polo <hernan.polo1985@gmail.com>
2023-06-17 13:07:28 -0300

    exercicio 6 finalizado

---
+++ exercicio_6_04/script.js
@@ -0,0 +1,10 @@
+/*Faça um programa em JavaScript que solicite ao usuário que digite
+ seu primeiro nome e seu sobrenome. Em seguida, eles devem utilizar 
+ operadores de string 
+para concatenar os dois nomes e exibir o nome completo na tela.*/
+
+   
+
+let nome=prompt('Informe seu nome')
+let sobrenome=prompt( 'Informe seu sobrenome')
+alert ('Seja bem-vindo ' + nome +' '+ sobrenome + '.')
